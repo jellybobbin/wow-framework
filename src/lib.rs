763b5e20223a1bf0eb0f9fb1ecf15f555e1c2b59
jellybobbin <jellybobbin@gmail.com>
2019-12-16 08:11:39 +0800

    change framework

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,18 @@
+#![feature(async_closure)]
+
 use std::convert::Infallible;
 use std::net::SocketAddr;
 use hyper::{Body, Request, Response, Server, Error};
 use hyper::service::{make_service_fn, service_fn};
-use futures::future::Future;
 
 
 #[tokio::main]
-pub async fn main() -> Result<(),Error>{
+pub async fn run() -> Result<(),Error>{
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     let make_svc = make_service_fn(|_conn| async {
-        // service_fn converts our function into a `Service`
-        Ok::<_, Infallible>(service_fn( async move |req:Request<Body>| {
-            println!("{:?}",req.uri());
+
+        Ok::<_, Infallible>(service_fn( async move |req| -> Result<Response<Body>, Infallible>{
+            dbg!(req.uri().path());
             Ok(Response::new("Hello, World".into()))
         }))
     });
